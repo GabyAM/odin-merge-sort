6f5e63440cf4ef546c20c014c19276cbcadcd65c
GabyAM <gdgabyam@gmail.com>
2023-12-03 10:58:14 -0300

    adds mergeSort function
    this function sorts an array in a recursive way, separating the array into halves to sort and then merge them.

---
+++ mergeSort.js
@@ -0,0 +1,34 @@
+function mergeSort(array) {
+	if (array.length === 1) {
+		return array;
+	} else {
+		const firstHalf = mergeSort(array.slice(0, array.length / 2));
+		const secondHalf = mergeSort(array.slice(array.length / 2));
+
+		let newArray = [];
+		let i = 0;
+		let j = 0;
+		while (firstHalf[i] !== undefined || secondHalf[j] !== undefined) {
+			let min;
+			if (firstHalf[i] === undefined) {
+				min = secondHalf.slice(j);
+				j = secondHalf.length + 1;
+			} else if (secondHalf[j] === undefined) {
+				min = firstHalf.slice(i);
+				i = firstHalf.length + 1;
+			} else {
+				if (firstHalf[i] < secondHalf[j]) {
+					min = firstHalf[i];
+					i++;
+				} else {
+					min = secondHalf[j];
+					j++;
+				}
+			}
+			newArray = newArray.concat(min);
+		}
+		return newArray;
+	}
+}
+
+console.log(mergeSort([2, 1, 5, 3, 8, 9, 4, 1]));
